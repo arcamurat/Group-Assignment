8f81fdbe54177a27c37cfb9f60b0a41153987e12
arcamurat <murat.akpek@ug.bilkent.edu.tr>
2024-02-07 17:38:21 +0300

    Updated displayMenu method

--- group.java
+++ group.java
@@ -1,53 +1,68 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class group {
-    int opt;
     int evenSum;
     int oddSum;
+    static int[] listOfNumbers;
 
     public static void main(String[] args) {
-        int[] lisOfNumbers = new int[5];
-        Random random = new Random();
-
-        for(int i=0; i<lisOfNumbers.length; i++){
-            lisOfNumbers[i] = random.nextInt(100);
-            System.out.println(lisOfNumbers[i] + " ");
-        }
-
-        
-
+        displayMenu();
     }
 
-
-    public void displayMenu(){
+    public static void displayMenu(){
+        int opt;
         do{
-            System.out.println("1) ");
+            System.out.println("\n\n1) Create the array");
             System.out.println("2) Find the minimum and maximum numbers");
-            System.out.println("3) ");
-            System.out.println("4) ");
+            System.out.println("3) Difference from average");
+            System.out.println("4) Sum of odd indexed and even indexed numbers");
             System.out.println("5) Exit");
 
             Scanner input = new Scanner(System.in);
-            System.out.println("Select an option: ");
-            int opt = input.nextInt();
+            System.out.print("Select an option: ");
+            opt = input.nextInt();
+
+            if(opt == 1){
+                listOfNumbers = createArrays();
+            }
+
+            if(opt == 2){
+                findMinumum(listOfNumbers);
+                findMaximum(listOfNumbers);
+            }
+
+            if(opt == 3){
+                System.out.println(Arrays.toString(differenceFromAverage(listOfNumbers)));
+            }
+
+            if(opt == 4){
+                System.out.printf("Sum of even indexes: %d", sumOfEvenIndexes(listOfNumbers));
+                System.out.printf("\nSum of odd indexes: %d", sumOfOddIndexes(listOfNumbers));
+            }
+
+            if(opt == 5){
+                System.out.println("\nExiting...");
+                System.out.println("Closed successfully.");
+            }
         }while(opt != 5);
     }
 
-    public void sumOf(int[] listNum){
-        for(int i = 0; i < listNum.length; i++){
-            if(listNum[i] % 2 == 0){
-                evenSum += listNum[i];
-            }
-            else{
-                oddSum += listNum[i];
-            }
+    public static int[] createArrays(){
+        int[] listOfNumbers = new int[5];
+        Random random = new Random();
+
+        for(int i=0; i<listOfNumbers.length; i++){
+            listOfNumbers[i] = random.nextInt(100);
+            System.out.print(listOfNumbers[i] + " ");
         }
-        System.out.printf("\nSum of even numbers: %d",evenSum);
-        System.out.printf("\nSum of even numbers: %d",oddSum);
+        System.out.println();
+        return listOfNumbers;
     }
 
-    public void findMaximum(int[] number) {
+
+    public static void findMaximum(int[] number) {
         int maxValue = Integer.MIN_VALUE;
         for (int i = 0; i < number.length; i++) {
             if (number[i] > maxValue) {
@@ -57,7 +72,7 @@ public class group {
         System.out.println("Maximum value of array is " + maxValue);
     }
     
-    public void findMinumum(int[] number) {
+    public static void findMinumum(int[] number) {
         int minValue = Integer.MAX_VALUE;
         for (int i = 0; i < number.length; i++) {
             if (number[i] < minValue) {
@@ -67,9 +82,9 @@ public class group {
         System.out.println("Minumum value of array is " + minValue);
     }
 
-    public int[] differenceFromAverage(int[] givenArray){
+    public static int[] differenceFromAverage(int[] givenArray){
         int[]differenceArray = new int[givenArray.length];
-        int total =0;
+        int total = 0;
         int average = 0;
 
             for(int i =0; i<givenArray.length;i++){
@@ -83,7 +98,7 @@ public class group {
         return differenceArray;
         }
 
-        public int sumOfEvenIndexes (int[] givenArray){
+    public static int sumOfEvenIndexes (int[] givenArray){
         int sumOfEvenIndex = 0;
         
         for(int i =0; i<givenArray.length;i++){
@@ -92,9 +107,9 @@ public class group {
             }
         }
         return sumOfEvenIndex;
-        }
+    }
 
-    public int sumOfOddIndexes (int[] givenArray){
+    public static int sumOfOddIndexes (int[] givenArray){
         int sumOfOddIndex = 0;
         for(int i =0; i<givenArray.length;i++){
             if(i%2 == 1){
@@ -103,7 +118,5 @@ public class group {
         }
         return sumOfOddIndex;
     }
-
-
 
 }
