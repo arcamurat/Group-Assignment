24eecb17465e7b1d935c412d0d527788a7b95200
Doğa Ertürk <doga.erturk@ug.bilkent.edu.tr>
2024-02-07 15:08:52 +0300

    given array

--- group.java
+++ group.java
@@ -1,17 +1,23 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class group {
-    int opt;
+    public static void main(String[] args) {
+        int[] lisOfNumbers = new int[5];
+        Random random = new Random();
 
+        for(int i=0; i<lisOfNumbers.length; i++){
+            lisOfNumbers[i] = random.nextInt(100);
+            System.out.println(lisOfNumbers[i] + " ");
+        }
 
-
-
+    }
 
 
     public void displayMenu(){
         do{
             System.out.println("1) ");
-            System.out.println("2) ");
+            System.out.println("2) Find the minimum and maximum numbers");
             System.out.println("3) ");
             System.out.println("4) ");
             System.out.println("5) Exit");
