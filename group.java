b958dec19167d561b5961192c849a999189a2e69
arcamurat <murat.akpek@ug.bilkent.edu.tr>
2024-02-07 14:56:12 +0300

    displayMenu() 0.1

--- group.java
+++ group.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class group {
+    int opt;
+
+
+
+
+
+
+    public void displayMenu(){
+        do{
+            System.out.println("1) ");
+            System.out.println("2) ");
+            System.out.println("3) ");
+            System.out.println("4) ");
+            System.out.println("5) Exit");
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Select an option: ");
+            int opt = input.nextInt();
+
+        }while(opt != 5);
+
+    }
+
+
+}
